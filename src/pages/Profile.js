d6d20d4e0d46dcfba71cb51b4fba92f96602f08e
unknown <superek127@gmail.com>
2019-10-19 22:13:50 +0900

    profile style modify

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,9 +9,9 @@ function Profile() {
         <h2>CHANGE</h2>
         <h2 className={"chance"}>CHANCE</h2>
       </div>
-      <div className={"slogan"}>
+      <div className={"description"}>
         <p>마인드의 변화를 갖지 않는다면 기회는 오지 않는다 </p>
-        <p>가장 잘 할 수 있는 일을, 가장 즐겁게 라는 신념을 바탕으로 경험의 계단을 통해 시행착오를 겪으며  노력만큼 빛나는 인생이 된다는 믿음을 갖고 있는 사람</p>
+        <p><blockquote className={"slogan"}>가장 잘 할 수 있는 일을, 가장 즐겁게</blockquote>라는 신념을 바탕으로 경험의 계단을 통해 시행착오를 겪으며  노력만큼 빛나는 인생이 된다는 믿음을 갖고 있는 사람</p>
       </div>
     </div>
   );
